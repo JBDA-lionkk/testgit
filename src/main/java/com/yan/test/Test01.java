58a57ab965143697025ab3266705699513ab7648
tom_glb <goodMorning_glb@atguigu.com>
2021-03-07 18:16:23 +0800

    se

---
+++ src/main/java/com/yan/test/Test01.java
@@ -0,0 +1,13 @@
+package com.yan.test;
+
+/**
+ * Test01
+ *
+ * @author 25288
+ * @date 2021/3/7 18:14
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
